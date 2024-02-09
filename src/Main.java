393d99b07d7228ec54cb82481700852d3886db75
projectTogithub <https://github@.com/YozeF-Bit/projectTogithub.git>
2024-02-09 17:48:01 +0300

    1:add rows

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
+
+        System.out.println("новая строка 1 ");
+        System.out.println("новая строка 2 ");
+
     }
 }
